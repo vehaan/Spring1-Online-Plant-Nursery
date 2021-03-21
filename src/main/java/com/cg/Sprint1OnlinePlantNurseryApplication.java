f621cec8dc0194443ab5c462549b09cbe35e4152
Vehaan Singh Kundra <vehaanskundra@gmail.com>
2021-03-21 20:04:15 +0530

    File logger added

--- src/main/java/com/cg/Sprint1OnlinePlantNurseryApplication.java
+++ src/main/java/com/cg/Sprint1OnlinePlantNurseryApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Sprint1OnlinePlantNurseryApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(Sprint1OnlinePlantNurseryApplication.class, args);
 	}
